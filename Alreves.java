4a9df9899e5aa9bf5e916330ddcf87d3c27d2ba3
Javier Martin Arroyo <93380199+JaviMartinA@users.noreply.github.com>
2021-12-10 12:30:14 +0100

    Create Alreves.java

---
+++ Alreves.java
@@ -0,0 +1,16 @@
+package com.primero;
+
+public class Alreves {
+
+	public static void main(String[] args) {
+		String cadena = "Buenos dias Carmelo";
+		String invertida = "";
+		for (int i = cadena.length() - 1; i >= 0; i--) {
+			invertida += cadena.charAt(i);
+			}
+			System.out.println("Cadena invertida: " + invertida);
+
+			}
+		
+
+	}
